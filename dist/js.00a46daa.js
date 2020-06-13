f36838df77b86b184490e724acd671990662550e
netero <netero@pop-os.localdomain>
2020-06-13 20:10:28 +0200

    try again folder rename

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -158,7 +158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44377" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43121" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -335,4 +335,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../.nvm/versions/node/v12.16.1/pnpm-global/3/node_modules/.pnpm/registry.npmjs.org/parcel-bundler/1.12.4/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
-//# sourceMappingURL=/single-price-grid/js.00a46daa.js.map
+//# sourceMappingURL=/dist/js.00a46daa.js.map
